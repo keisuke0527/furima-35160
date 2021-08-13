c1ab1e9bcce4ade1bb7c5794a2183c02bd608bf3
keisuke0527 <okamonton@gmail.com>
2021-08-13 18:49:10 +0900

    ヘルパーメゾットの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :set_item,  only: [:show, :edit,:update ]
   before_action :authenticate_user!, except: [:index,:show]
+  before_action :contributor_confirmation, only: [:edit, :update ]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -23,13 +25,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
   end
 
   def update
-       @item = Item.find(params[:id])
     if @item.update(item_params)
-     redirect_to edit_item_path
+     redirect_to root_path
     else
      render :edit
     end
@@ -42,4 +43,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :item_name, :description, :category_id, :status_id, :delivery_charge_id, :area_id,
                                  :scheduled_day_id, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user ==  @item.user
+  end
+
 end
