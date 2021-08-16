338d15e80d0b5ec1300ca249793a8ab783bbe485
keisuke0527 <okamonton@gmail.com>
2021-08-16 14:19:33 +0900

    購入画面表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
   end
  
   def new
-    @order_recidence = OrderRecidence.new   #「UserDonation」に編集
+    @order_recidence = OrderRecidence.new  
   end
  
   def create
@@ -12,7 +12,7 @@ class OrdersController < ApplicationController
   end
  
   private
-   # 全てのストロングパラメーターを1つに統合
+  
   def donation_params
    params.require(:user_donation).permit(:name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price)
   end
