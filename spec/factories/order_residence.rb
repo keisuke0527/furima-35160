abf520e8ac187e833964dcc51b7baaf998700283
keisuke0527 <okamonton@gmail.com>
2021-08-18 00:27:28 +0900

    単体モデル

---
+++ spec/factories/order_residence.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order_residence do
+    postal_code      { '123-4567' }
+    address          { '1-1'}
+    phone_number     { '09012345678' }
+    city             { '名古屋' }
+    building_name    { 'ビルビル' }
+    area_id          { 2 }
+  end
+end
+
---
+++ spec/models/order_residence_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe OrderResidence, type: :model do
+  describe '#create' do
+  end
+
+  before do
+    @order_residence = FactoryBot.build(:order_residence)
+  end
+
+  describe '商品購入機能' do
+    context '購入できるとき' do
+      it '必要な情報を適切に入力すると、商品の購入ができること' do
+        expect(@order_residence).to be_valid
+      end
+      it 'ログイン状態のユーザーのみ、商品出品ページへ遷移できること' do
+        @order_residence = FactoryBot.create(:user)
+        expect(@order_residence).to be_valid
+      end
+     end
+
+     context '新規登録できないとき' do
+
+      it '郵便番号が空では登録できない' do
+        @order_residence.postal_code = nil
+        @order_residence.valid?
+        expect(@order_residence.errors.full_messages).to include("Postal code can't be blank", "Postal code is invalid")
+      end
+      it '都道府県が空では登録できない' do
+        @order_residence.area_id = nil
+        @order_residence.valid?
+        expect(@order_residence.errors.full_messages).to include("Area can't be blank", "Area is not a number")
+      end
+      it '市区町村が空では登録できない' do
+        @order_residence.city = nil
+        @order_residence.valid?
+        expect(@order_residence.errors.full_messages).to include("City can't be blank")
+      end
+      it '番地が空では登録できない' do
+        @order_residence.address = nil
+        @order_residence.valid?
+        expect(@order_residence.errors.full_messages).to include("Address can't be blank")
+      end
+      it '電話番号が空では登録できない' do
+        @order_residence.phone_number = nil
+        @order_residence.valid?
+        expect(@order_residence.errors.full_messages).to include("Phone number can't be blank", "Phone number is invalid")
+      end
+      it '郵便番号の保存にはハイフンがないと登録できない' do
+        @order_residence.postal_code = '1234567'
+        @order_residence.valid?
+        expect(@order_residence.errors.full_messages).to include("Postal code is invalid")
+      end
+      it '電話番号は11桁以内の数値でないと登録できない' do
+        @order_residence.phone_number = '12345'
+        @order_residence.valid?
+        expect(@order_residence.errors.full_messages).to include("Phone number is invalid")
+      end
+    end
+  end
+  end
+
